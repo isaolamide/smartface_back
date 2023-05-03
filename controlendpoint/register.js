7997af7787fe3434a1b81a22d224dcf32ee064e6
isaolamide <isaolamide@gmail.com>
2023-05-03 16:46:41 -0700

    corrrecting register

--- controlendpoint/register.js
+++ controlendpoint/register.js
@@ -1,6 +1,6 @@
 const handleRegister= (req,res, db, bcrypt)=>{
     const {name, email, password}= req.body
-    if (!email || !name || password ){
+    if (!email || !name || !password ){
         return res.status(400).json('incorrect submission')
     }
     const hash = bcrypt.hashSync(password);
